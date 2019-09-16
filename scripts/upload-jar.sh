bf392a1ced1305412fcd54003a46bd0ca5dfba5e
Lawrence <github@lawben.com>
2019-09-16 14:57:50 +0200

    Update script

---
+++ scripts/upload-jar.sh
@@ -0,0 +1,10 @@
+set -e
+
+FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BASE_DIR="$(cd $FILE_DIR/.. && pwd)"
+cd "$BASE_DIR"
+
+./gradlew shadowJar > /dev/null
+
+scp benchmarks/build/libs/benchmarks-1.0-SNAPSHOT-all.jar benson@sshgate:disco.jar
+ssh benson@sshgate "scp disco.jar hadoop@cloud-11.dima.tu-berlin.de:benson/disco.jar"
