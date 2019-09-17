1e62823fdcf6bd045b9e86e975315715da82d555
Lawrence <github@lawben.com>
2019-09-17 10:26:41 +0200

    Update init script for new cluster

--- scripts/upload-jar.sh
+++ scripts/upload-jar.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -e
 
 FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -7,4 +8,4 @@ cd "$BASE_DIR"
 ./gradlew shadowJar > /dev/null
 
 scp benchmarks/build/libs/benchmarks-1.0-SNAPSHOT-all.jar benson@sshgate:disco.jar
-ssh benson@sshgate "scp disco.jar hadoop@cloud-11.dima.tu-berlin.de:benson/disco.jar"
+ssh benson@sshgate "scp disco.jar hadoop@cloud-11.dima.tu-berlin.de:/share/hadoop/benson/disco.jar"
