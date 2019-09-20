5d11fcba6df0ad5ef43ca2e3575d9f2bc3dda489
Lawrence <github@lawben.com>
2019-09-21 00:15:41 +0200

    Fix matrix call

--- benchmarks/scripts/sustainable-throughput-bm.py
+++ benchmarks/scripts/sustainable-throughput-bm.py
@@ -1,15 +1,17 @@
+from typing import List, Tuple
+
 from executables.latency import run_latency
 from executables.find_sustainable_throughput import run_throughput
 
 DURATION = 120
 
 
-def _run_single_benchmark(num_children, num_streams, windows, agg_fns):
+def _run_single_benchmark(num_children: int, num_streams: int, windows: str, agg_fns: str):
     throughput = run_throughput(num_children, num_streams, DURATION, windows, agg_fns)
     run_latency(num_children, num_streams, throughput, DURATION, windows, agg_fns)
 
 
-def run_benchmark(windows, agg_fns, num_nodes_config):
+def run_benchmark(windows: str, agg_fns: str, num_nodes_config: List[Tuple[int, int]]):
     print(f"BENCHMARK: WINDOWS: {windows} - AGG_FNS: {agg_fns}")
     for num_children, num_streams in num_nodes_config:
         child_str = "child" if num_children == 1 else "children"
@@ -18,15 +20,21 @@ def run_benchmark(windows, agg_fns, num_nodes_config):
         _run_single_benchmark(num_children, num_streams, windows, agg_fns)
 
 
+def run_benchmark_matrix(windows: List[str], agg_fns: List[str], node_config: List[Tuple[int, int]]):
+    for window in windows:
+        for agg_fn in agg_fns:
+            run_benchmark(window, agg_fn, node_config)
+
+
 def run_all():
-    windows = ["TUMBLING,1000", "SLIDING,1000,500"]  # , "SESSION,100"]
-    agg_fns = ["MAX", "M_AVG", "M_MEDIAN"]
-    node_config = [(1, 1), (1, 2), (1, 4), (1, 8), (2, 2), (4, 4), (8, 8)]
-    run_benchmark(windows, agg_fns, node_config)
+    basic_windows = ["TUMBLING,1000", "SLIDING,1000,500"]  # , "SESSION,100"]
+    basic_agg_fns = ["MAX", "M_AVG", "M_MEDIAN"]
+    basic_node_config = [(1, 1), (1, 2), (1, 4), (1, 8), (2, 2), (4, 4), (8, 8)]
+    run_benchmark_matrix(basic_windows, basic_agg_fns, basic_node_config)
 
     ############################
 
-    windows = [
+    tumbling_windows = [
         # 10-10000 equal tumbling windows
         "\n".join([f"TUMBLING,1000,{i}" for i in range(5)]),
         "\n".join([f"TUMBLING,1000,{i}" for i in range(10)]),
@@ -37,9 +45,9 @@ def run_all():
         "\n".join([f"TUMBLING,1000,{i}" for i in range(5000)]),
         "\n".join([f"TUMBLING,1000,{i}" for i in range(10000)])
     ]
-    agg_fns = ["MAX", "M_MEDIAN"]
-    node_config = [(1, 1), (2, 2), (4, 4), (8, 8)]
-    run_benchmark(windows, agg_fns, node_config)
+    tumbling_agg_fns = ["MAX", "M_MEDIAN"]
+    tumbling_node_config = [(1, 1), (2, 2), (4, 4), (8, 8)]
+    run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
 
 
 if __name__ == '__main__':
