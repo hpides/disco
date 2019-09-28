af8dc2f3779ac0879bf1e4e39e73b136b984748a
Lawrence <github@lawben.com>
2019-09-28 18:18:01 +0200

    Fix scripts for new multi-level format

--- benchmarks/scripts/executables/latency.py
+++ benchmarks/scripts/executables/latency.py
@@ -1,18 +1,19 @@
 from argparse import ArgumentParser
 from multiprocessing import Pipe, Process
+from typing import List
 
 from lib.common import logs_are_unsustainable
 from lib.run import run as run_all_main
 
 
-def single_latency_run(num_children, num_streams, num_events, duration,
-                       windows, agg_functions):
-    num_nodes = num_children + num_streams + 1  # + 1 for root
+def single_latency_run(node_config: List[int], num_events: int, duration: int,
+                       windows: str, agg_functions: str):
+    num_nodes = sum(node_config) + 1  # + 1 for root
     timeout = duration + 30
     print(f"Running latency test with {num_events} events/s.")
     process_recv_pipe, process_send_pipe = Pipe(False)
     run_process = Process(target=run_all_main,
-                          args=(num_children, num_streams,
+                          args=(node_config,
                                 num_events, duration, windows,
                                 agg_functions, process_send_pipe),
                           name=f"process-run-{num_nodes}-{num_events}")
@@ -28,14 +29,14 @@ def single_latency_run(num_children, num_streams, num_events, duration,
     return logs_are_unsustainable(log_directory), log_directory
 
 
-def _latency_run(num_children, num_streams, num_events, duration,
-                 windows, agg_functions):
+def _latency_run(node_config: List[int], num_events: int, duration: int,
+                 windows: str, agg_functions: str):
     is_unsustainable = None
     log_directory = None
     tries = 0
     while (is_unsustainable is None or is_unsustainable) and tries < 3:
         is_unsustainable, log_directory = \
-            single_latency_run(num_children, num_streams, num_events,
+            single_latency_run(node_config, num_events,
                                duration, windows, agg_functions)
 
         tries += 1
@@ -53,21 +54,21 @@ def _latency_run(num_children, num_streams, num_events, duration,
     print(f"Latencies in dir: {log_directory}")
 
 
-def run_latency(num_children, num_streams, num_events, duration,
-                windows, agg_functions):
+def run_latency(node_config: List[int], num_events: int, duration: int,
+                windows: str, agg_functions: str):
     quarter_events = num_events // 4
     print(f"Running with quarter events/s: {quarter_events}")
-    _latency_run(num_children, num_streams, quarter_events,
+    _latency_run(node_config, quarter_events,
                  duration, windows, agg_functions)
 
     half_events = quarter_events * 2
     print(f"Running with half events/s: {half_events}")
-    _latency_run(num_children, num_streams, half_events,
+    _latency_run(node_config, half_events,
                  duration, windows, agg_functions)
 
     three_quarter_events = quarter_events * 3
     print(f"Running with three quarter events/s: {three_quarter_events}")
-    _latency_run(num_children, num_streams, three_quarter_events,
+    _latency_run(node_config, three_quarter_events,
                  duration, windows, agg_functions)
 
     # Ignore for now and use last sustainable run
@@ -77,15 +78,16 @@ def run_latency(num_children, num_streams, num_events, duration,
     #              duration, windows, agg_functions)
 
 
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("--num-children", type=int, required=True, dest='num_children')
-    parser.add_argument("--num-streams", type=int, required=True, dest='num_streams')
-    parser.add_argument("--num-events", type=int, required=True, dest="num_events")
-    parser.add_argument("--duration", type=int, dest='duration', default=120)
-    parser.add_argument("--windows", type=str, required=True, dest="windows")
-    parser.add_argument("--agg-functions", type=str, required=True, dest="agg_functions")
-
-    args = parser.parse_args()
-    run_latency(args.num_children, args.num_streams, args.num_events,
-                args.duration, args.windows, args.agg_functions)
+# TODO: change if needed again
+# if __name__ == '__main__':
+#     parser = ArgumentParser()
+#     parser.add_argument("--num-children", type=int, required=True, dest='num_children')
+#     parser.add_argument("--num-streams", type=int, required=True, dest='num_streams')
+#     parser.add_argument("--num-events", type=int, required=True, dest="num_events")
+#     parser.add_argument("--duration", type=int, dest='duration', default=120)
+#     parser.add_argument("--windows", type=str, required=True, dest="windows")
+#     parser.add_argument("--agg-functions", type=str, required=True, dest="agg_functions")
+#
+#     args = parser.parse_args()
+#     run_latency(args.num_children, args.num_streams, args.num_events,
+#                 args.duration, args.windows, args.agg_functions)
--- benchmarks/scripts/sustainable-throughput-bm.py
+++ benchmarks/scripts/sustainable-throughput-bm.py
@@ -6,21 +6,23 @@ from executables.find_sustainable_throughput import run_throughput
 DURATION = 120
 
 
-def _run_single_benchmark(num_children: int, num_streams: int, windows: str, agg_fns: str):
-    throughput = run_throughput(num_children, num_streams, DURATION, windows, agg_fns)
-    run_latency(num_children, num_streams, throughput, DURATION, windows, agg_fns)
+def _run_single_benchmark(node_config: List[int], windows: str, agg_fns: str):
+    throughput = run_throughput(node_config, DURATION, windows, agg_fns)
+    run_latency(node_config, throughput, DURATION, windows, agg_fns)
 
 
-def run_benchmark(windows: str, agg_fns: str, num_nodes_config: List[Tuple[int, int]]):
+def run_benchmark(windows: str, agg_fns: str, node_configs: List[List[int]]):
     print(f"BENCHMARK: WINDOWS: {windows} - AGG_FNS: {agg_fns}")
-    for num_children, num_streams in num_nodes_config:
-        child_str = "child" if num_children == 1 else "children"
-        stream_str = "stream" if num_streams == 1 else "streams"
-        print(f"Running {num_children} {child_str}, {num_streams} {stream_str}")
-        _run_single_benchmark(num_children, num_streams, windows, agg_fns)
+    for node_config in node_configs:
+        intermediates = "-".join([str(x) for x in node_config[:-2]])
+        intermediates_str = (f"{intermediates}" if intermediates else "0") + " intermediates"
+        children_str = f"{node_config[-2]} children"
+        streams_tr = f"{node_config[-1]} streams"
+        print(f"Running {intermediates_str}, {children_str}, {streams_tr}")
+        _run_single_benchmark(node_config, windows, agg_fns)
 
 
-def run_benchmark_matrix(windows: List[str], agg_fns: List[str], node_config: List[Tuple[int, int]]):
+def run_benchmark_matrix(windows: List[str], agg_fns: List[str], node_config: List[List[int]]):
     for agg_fn in agg_fns:
         for window in windows:
             run_benchmark(window, agg_fn, node_config)
@@ -29,7 +31,7 @@ def run_benchmark_matrix(windows: List[str], agg_fns: List[str], node_config: Li
 def run_all():
     # basic_windows = ["TUMBLING,1000", "SLIDING,1000,500"]  # , "SESSION,100"]
     # basic_agg_fns = ["MAX", "M_AVG", "M_MEDIAN"]
-    # basic_node_config = [(1, 1), (1, 2), (1, 4), (1, 8), (2, 2), (4, 4), (8, 8)]
+    # basic_node_config = [[1, 1], [1, 2], [1, 4], [1, 8], [2, 2], [4, 4], [8, 8]]
     # run_benchmark_matrix(basic_windows, basic_agg_fns, basic_node_config)
 
     ############################
@@ -45,7 +47,7 @@ def run_all():
         "CONCURRENT,1000,TUMBLING,1000",
     ]
     tumbling_agg_fns = ["M_AVG"]
-    tumbling_node_config = [(1, 1)]
+    tumbling_node_config = [[1, 1]]
     run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
 
     tumbling_windows = [
@@ -53,7 +55,7 @@ def run_all():
         "CONCURRENT,1000,TUMBLING,1000",
     ]
     tumbling_agg_fns = ["MAX"]
-    tumbling_node_config = [(1, 1)]
+    tumbling_node_config = [[1, 1]]
     run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
 
     tumbling_windows = [
@@ -64,7 +66,7 @@ def run_all():
         "CONCURRENT,1000,TUMBLING,1000",
     ]
     tumbling_agg_fns = ["M_MEDIAN"]
-    tumbling_node_config = [(1, 1)]
+    tumbling_node_config = [[1, 1]]
     run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
 
 
