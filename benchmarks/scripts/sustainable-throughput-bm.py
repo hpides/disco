ed48bf552d4450af2646fa837f09ec673c7538fe
Lawrence <github@lawben.com>
2019-09-30 15:43:15 +0200

    Add single node run

--- benchmarks/scripts/sustainable-throughput-bm.py
+++ benchmarks/scripts/sustainable-throughput-bm.py
@@ -30,45 +30,27 @@ def run_single_node_benchmark_matrix(windows: List[str], agg_fns: List[str], nod
 
 
 def run_all():
-    # basic_windows = ["TUMBLING,1000", "SLIDING,1000,500"]  # , "SESSION,100"]
-    # basic_agg_fns = ["MAX", "M_AVG", "M_MEDIAN"]
-    # basic_node_config = [[1, 1], [1, 2], [1, 4], [1, 8], [2, 2], [4, 4], [8, 8]]
+    basic_windows = ["TUMBLING,1000", "SLIDING,1000,500"]  # , "SESSION,100"]
+    basic_agg_fns = ["MAX", "M_AVG", "M_MEDIAN"]
+    basic_node_config = [[1, 1], [1, 2], [1, 4], [1, 8], [2, 2], [4, 4], [8, 8]]
     # run_benchmark_matrix(basic_windows, basic_agg_fns, basic_node_config)
+    run_single_node_benchmark_matrix(basic_windows, basic_agg_fns, basic_node_config)
 
     ############################
 
-    tumbling_windows = [
-        # 1-1000 overlapping tumbling windows
-        "CONCURRENT,1,TUMBLING,1000",
-        "CONCURRENT,5,TUMBLING,1000",
-        "CONCURRENT,10,TUMBLING,1000",
-        "CONCURRENT,50,TUMBLING,1000",
-        "CONCURRENT,100,TUMBLING,1000",
-        "CONCURRENT,500,TUMBLING,1000",
-        "CONCURRENT,1000,TUMBLING,1000",
-    ]
-    tumbling_agg_fns = ["M_AVG"]
-    tumbling_node_config = [[1, 1]]
-    run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
-
-    tumbling_windows = [
-        "CONCURRENT,500,TUMBLING,1000",
-        "CONCURRENT,1000,TUMBLING,1000",
-    ]
-    tumbling_agg_fns = ["MAX"]
-    tumbling_node_config = [[1, 1]]
-    run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
-
-    tumbling_windows = [
-        "CONCURRENT,10,TUMBLING,1000",
-        "CONCURRENT,50,TUMBLING,1000",
-        "CONCURRENT,100,TUMBLING,1000",
-        "CONCURRENT,500,TUMBLING,1000",
-        "CONCURRENT,1000,TUMBLING,1000",
-    ]
-    tumbling_agg_fns = ["M_MEDIAN"]
-    tumbling_node_config = [[1, 1]]
-    run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
+    # tumbling_windows = [
+    #     # 1-1000 overlapping tumbling windows
+    #     "CONCURRENT,1,TUMBLING,1000",
+    #     "CONCURRENT,5,TUMBLING,1000",
+    #     "CONCURRENT,10,TUMBLING,1000",
+    #     "CONCURRENT,50,TUMBLING,1000",
+    #     "CONCURRENT,100,TUMBLING,1000",
+    #     "CONCURRENT,500,TUMBLING,1000",
+    #     "CONCURRENT,1000,TUMBLING,1000",
+    # ]
+    # tumbling_agg_fns = ["MAX", "M_AVG", "M_MEDIAN"]
+    # tumbling_node_config = [[1, 1]]
+    # run_benchmark_matrix(tumbling_windows, tumbling_agg_fns, tumbling_node_config)
 
 
 if __name__ == '__main__':
