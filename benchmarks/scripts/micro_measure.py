dc9f19af4799f30c1af69912b5b161c65d7f1f92
Lawrence <github@lawben.com>
2019-09-30 09:55:50 +0200

    Update micro-measure throughputs

--- benchmarks/scripts/micro_measure.py
+++ benchmarks/scripts/micro_measure.py
@@ -2,9 +2,9 @@ from lib.run import run_single_level
 
 
 def micro_measure():
-    run_single_level(1, 1, 1500000, 120, "TUMBLING,1000", "MAX")
-    run_single_level(1, 1, 1500000, 120, "TUMBLING,1000", "M_AVG")
-    run_single_level(1, 1,   75000, 120, "TUMBLING,1000", "M_MEDIAN")
+    run_single_level(1, 1, 1000000, 120, "TUMBLING,1000", "MAX")
+    run_single_level(1, 1,  900000, 120, "TUMBLING,1000", "M_AVG")
+    run_single_level(1, 1,   30000, 120, "TUMBLING,1000", "M_MEDIAN")
 
 
 if __name__ == '__main__':
